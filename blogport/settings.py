5f5ef93d117801a649432cc5b3adab1b809fd3fb
netphixs <okpalachidiebere99@gmail.com>
2024-04-12 19:59:41 -0700

    initial commit

--- blogport/settings.py
+++ blogport/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7raxc+p*t8!xrl#f2fnc*d@dn9w9y6ycpi_az=zg0m(n!x*5nb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-portforlio-app-0efb61176ceb.herokuapp.com']
 
 
 
